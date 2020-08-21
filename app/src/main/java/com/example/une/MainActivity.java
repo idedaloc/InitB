016def7396ca70d5fde6e4e3986cb1d3b929321e
Jorge Garcia <ja.garcia1dev@gmail.com>
2020-08-21 23:46:18 +0000

    Temporal messaging layout conf

--- app/src/main/java/com/example/une/MainActivity.java
+++ app/src/main/java/com/example/une/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.une;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.une.Home.HomeFragmentDirections;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView menu = findViewById(R.id.navigationView);
+        menu.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.homeMenu:
+                        Navigation.createNavigateOnClickListener(R.id.home);
+                        break;
+                    case R.id.searchMenu:
+                        Navigation.createNavigateOnClickListener(R.id.search);
+                        break;
+                    case R.id.recomendationsMenu:
+                        Navigation.createNavigateOnClickListener(R.id.recommendations);
+                        break;
+                }
+            }
+        });
+
     }
 
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
